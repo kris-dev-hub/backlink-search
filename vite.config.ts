058a69e62be55a3466143568dcee9f010b8fa8a8
kris-dev-hub <kris@kris.biz.pl>
2025-09-28 00:09:34 +0200

    newdesign tests

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/dev/backlink-search'
+  base: process.env.VITE_BASE_PATH || '/dev/backlink-search'
 })
